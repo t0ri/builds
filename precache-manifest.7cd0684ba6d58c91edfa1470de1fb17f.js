e5f01ea70f390528748b835ac04a8443d4219b4e
t0ri <victoriarosemurray@gmail.com>
2019-08-16 16:46:10 -0700

    Updates

---
+++ precache-manifest.7cd0684ba6d58c91edfa1470de1fb17f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f3e9aff751e2ab27285233ff3cde12",
+    "url": "/builds/index.html"
+  },
+  {
+    "revision": "afa89c8740e3da2fd8a7",
+    "url": "/builds/static/css/main.3fa9fbc2.chunk.css"
+  },
+  {
+    "revision": "f48ea665144d9aa3fc89",
+    "url": "/builds/static/js/2.fa5d9d45.chunk.js"
+  },
+  {
+    "revision": "afa89c8740e3da2fd8a7",
+    "url": "/builds/static/js/main.7ba7fc47.chunk.js"
+  },
+  {
+    "revision": "f68bebf442e89934ca25",
+    "url": "/builds/static/js/runtime~main.54089324.js"
+  }
+]);
